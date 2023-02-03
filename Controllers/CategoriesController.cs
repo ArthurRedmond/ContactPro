d0efe49c276d94fc691fcbd699c5ce0cacb6b033
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-03 16:13:48 -0500

    Modify Category create view

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -9,6 +9,7 @@ using ContactPro.Data;
 using ContactPro.Models;
 using Microsoft.AspNetCore.Identity;
 using ContactPro.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ContactPro.Controllers
 {
@@ -57,9 +58,9 @@ namespace ContactPro.Controllers
         }
 
         // GET: Categories/Create
+        [Authorize]
         public IActionResult Create()
         {
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id");
             return View();
         }
 
@@ -70,13 +71,16 @@ namespace ContactPro.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,AppUserId,Name")] Category category)
         {
+            ModelState.Remove("AppUserId");
             if (ModelState.IsValid)
             {
+                string appUserId = _userManager.GetUserId(User);
+                category.AppUserId = appUserId;
                 _context.Add(category);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["AppUserId"] = new SelectList(_context.Users, "Id", "Id", category.AppUserId);
+            
             return View(category);
         }
 
