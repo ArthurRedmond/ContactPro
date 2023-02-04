30b1cc07b1951150060ffc2f963ce84beefea8bd
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-03 21:08:12 -0500

    Removed details action and view

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -88,26 +88,7 @@ namespace ContactPro.Controllers
             }
             return View(ecvm);
         }
-
-        // GET: Categories/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Categories == null)
-            {
-                return NotFound();
-            }
-
-            var category = await _context.Categories
-                .Include(c => c.AppUser)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (category == null)
-            {
-                return NotFound();
-            }
-
-            return View(category);
-        }
-
+              
         // GET: Categories/Create
         [Authorize]
         public IActionResult Create()
