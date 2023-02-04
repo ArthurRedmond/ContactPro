e32eeb61828c38d8c8b5cb78cd22f84f487de0d7
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-02-03 20:45:23 -0500

    Added category group email to controller

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -10,6 +10,8 @@ using ContactPro.Models;
 using Microsoft.AspNetCore.Identity;
 using ContactPro.Models.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using ContactPro.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace ContactPro.Controllers
 {
@@ -17,19 +19,23 @@ namespace ContactPro.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<AppUser> _userManager;
+        private readonly IEmailSender _emailService;
 
         public CategoriesController(ApplicationDbContext context,
-                                    UserManager<AppUser> userManager)
+                                    UserManager<AppUser> userManager,
+                                    IEmailSender emailService)
         {
             _context = context;
             _userManager = userManager;
+            _emailService = emailService;
         }
 
         // GET: Categories
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string swalMessage = null)
         {
-            string appUserId = _userManager.GetUserId(User);
+            ViewData["SwalMessage"] = swalMessage;
 
+            string appUserId = _userManager.GetUserId(User);
 
             var categories = await _context.Categories.Where(c => c.AppUserId == appUserId)
                                                 .Include(c => c.AppUser)
@@ -61,6 +67,26 @@ namespace ContactPro.Controllers
             };
 
             return View(model);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> EmailCategory(EmailCategoryViewModel ecvm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _emailService.SendEmailAsync(ecvm.EmailData.EmailAddress, ecvm.EmailData.Subject, ecvm.EmailData.Body);
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Success: Email Sent" });
+                }
+                catch (Exception ex)
+                {
+                    return RedirectToAction("Index", "Categories", new { swalMessage = "Error: Email Send Failed!" });
+                    throw;
+                }
+            }
+            return View(ecvm);
         }
 
         // GET: Categories/Details/5
